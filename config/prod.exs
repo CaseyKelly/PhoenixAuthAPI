93a6c8ad3fac55b04ddd1035340906c36a9f8e0a
Casey Kelly <casey.kelly@colorado.edu>
2016-05-20 15:47:32 -0400

    Update production configuration

--- config/prod.exs
+++ config/prod.exs
@@ -60,6 +60,12 @@ config :logger, level: :info
 #
 #     config :my_app, MyApp.Endpoint, root: "."
 
-# Finally import the config/prod.secret.exs
-# which should be versioned separately.
-import_config "prod.secret.exs"
+# Configure secret_key_base for cookie-based session storage
+  config :my_app, MyApp.Endpoint,
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
+
+# Configure your database
+  config :my_app, MyApp.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  size: 20 # The amount of database connections in the pool
